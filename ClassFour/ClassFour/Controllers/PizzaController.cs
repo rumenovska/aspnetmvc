f3e132a12a5267be2853b0613b000755af0e8d27
Marija Rumenovska <marija.rumenovska@hotmail.com>
2019-06-13 20:37:22 +0200

    Routes and Validation

---
+++ ClassFour/ClassFour/Controllers/PizzaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassFour.Controllers
+{
+   [Route("seavus-g1")]
+    [Route("[controller]")]
+    public class PizzaController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("menu")]
+        public IActionResult Menu()
+        {
+            return View();
+        }
+        [Route("offers")]
+        public IActionResult Offers()
+        {
+            return View();
+        }
+    }
+}
